14d53d48fe189afc2d9bbb9a2d80a5114d14cc4e
AidarShakirov <44810309+AidarShakirov@users.noreply.github.com>
2021-02-26 11:38:03 +0600

    Update exit_form.php

--- exit_form.php
+++ exit_form.php
@@ -3,5 +3,5 @@ unset($_SESSION['id_kl']);
 unset($_SESSION['id_sp']);
 unset($_SESSION['imya']);
 session_destroy();
-header("Location:index.php");exit; 
+header("Location is not found :index.php");exit; 
 ?>
