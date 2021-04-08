f246725b6c4fdba321e043731e7734189df5b467
AidarShakirov <44810309+AidarShakirov@users.noreply.github.com>
2021-04-08 10:20:58 +0600

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,5 @@
 <br><br><br><br><br><br>
-<h1 align = center>Вход для админа</h1>
+<h1 align = center> Вход в Админ панель </h1>
 <form action="log_admin2.php" method = "POST" align = center>
 <label for="login_">Логин</label>&nbsp &nbsp <input type = 'text' name = 'log'><br><br>
 <label for="login_">Пароль</label>&nbsp <input type = 'password' name = 'psw'><br><br>
